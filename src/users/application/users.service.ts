c1d68c6ceebfd410545fd01bddf04a773049f4ee
D1elit <dropdox12@gmail.com>
2025-10-12 17:34:17 +0200

    feat: added validation for unique email/login

--- src/users/application/users.service.ts
+++ src/users/application/users.service.ts
@@ -1,8 +1,11 @@
 import { UserInput } from '../router/input/user.input';
 import { usersRepository } from '../repositories/users.repository';
+import {usersQueryRepository} from "../repositories/users.query-repository";
+import {RepositoryNotFoundError} from "../../core/errors/repostory-not-found.error";
 
 export const usersService = {
   async create(userDto: UserInput) {
+    await usersService.ensureIsUserUnique(userDto.login, userDto.email);
     const newUser = {
       login: userDto.login,
       password: userDto.password,
@@ -15,4 +18,12 @@ export const usersService = {
   async delete(id: string) {
     return usersRepository.delete(id);
   },
+
+  async ensureIsUserUnique(login:string, email:string) {
+    let resLogin = await usersQueryRepository.findByField('login', login);
+    let resEmail = await usersQueryRepository.findByField('email', email);
+    if (resLogin || resEmail) {
+      throw new RepositoryNotFoundError('Login or email already exist');
+    }
+  }
 };
--- src/users/repositories/users.query-repository.ts
+++ src/users/repositories/users.query-repository.ts
@@ -2,6 +2,10 @@ import { postsCollection, usersCollection } from '../../db/mongo.db';
 import { UsersQueryInput } from '../router/input/user-query.input';
 import { mapToPostListPaginated } from '../router/mappers/map-to-list-paginated';
 import { UsersPaginatedOutput } from '../router/output/users-paginated.output';
+import {RepositoryNotFoundError} from "../../core/errors/repostory-not-found.error";
+import {UserOutput} from "../router/output/user.output";
+import {WithId} from "mongodb";
+import {User} from "../types/user";
 
 export const usersQueryRepository = {
   async findAll(queryDto: UsersQueryInput): Promise<UsersPaginatedOutput> {
@@ -27,4 +31,7 @@ export const usersQueryRepository = {
     const totalCount = await usersCollection.countDocuments(filter);
     return mapToPostListPaginated(users, { pageNumber, pageSize, totalCount });
   },
+  async findByField (fieldName: string, fieldValue: string): Promise<WithId<User> | null>  {
+    return  await usersCollection.findOne({[fieldName]: fieldValue})
+  }
 };
